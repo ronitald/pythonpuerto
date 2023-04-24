6b030e38cfbb821584ae93fbaa6b9e4e2c9e346a
ronald <ronaldpuerto65@gmail.com>
2023-04-24 17:31:15 -0500

    Ejercicio sin condicionales

---
+++ tarea/numero_mayor.py
@@ -0,0 +1,13 @@
+num1 = int(input("Ingrese el primer numero: "))
+num2 = int(input("Ingrese el segundo numero: "))
+num3 = int(input("Ingrese el tercer numero: "))
+
+if num1 >= num2:
+    if num1 >= num3:
+        print("El número más grande es: ",num1)
+    else:
+        num2 >= num3
+        print("El numero mas grande es: ",num3)
+else:
+    if num2 >= num3:
+        print("El numero mas grande es: ",num2)
