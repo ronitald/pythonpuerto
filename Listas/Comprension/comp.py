e624e3d279af6afb64492bcfc7f37b7001184b44
ronald <ronaldpuerto65@gmail.com>
2023-05-10 13:13:16 -0500

    Tema comprension y rebanada correccion.

--- Listas/Comprension/comp.py
+++ Listas/Comprension/comp.py
@@ -1,4 +1,13 @@
 import random
-lista = [round (random.random ()*5,1)for i in range (20,30)]
+import math
 
-print (lista)
+tam = random.randint(5, 10)
+lista = [random.randrange(100) for i in range(tam)]
+lista2 = [i for i in lista]
+exponente = 2
+potencia = [x ** exponente for x in lista if x % 2 != 0]
+raices = [round(math.sqrt(x), 2) for x in lista2 if x % 2 == 0]
+
+print(lista)
+print(f'La potencia de los números impares es: {potencia}')
+print(f'La raíz de los números pares es: {raices}')
