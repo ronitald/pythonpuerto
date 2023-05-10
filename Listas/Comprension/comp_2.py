e775eb47789d08461680a8e6c627ea04ccd6424b
ronald <ronaldpuerto65@gmail.com>
2023-05-10 12:04:07 -0500

    Tema comprension y rebanada correccion.

--- Listas/Comprension/comp_2.py
+++ Listas/Comprension/comp_2.py
@@ -1,13 +1,34 @@
 import random
 import math
 
-tam = random.randint(5, 10)
-lista = [random.randrange(100) for i in range(tam)]
-lista2 = [i for i in lista]
-exponente = 2
-potencia = [x ** exponente for x in lista if x % 2 != 0]
-raices = [round(math.sqrt(x), 2) for x in lista2 if x % 2 == 0]
+tam = random.randrange(20, 30)
+notas = [round(random.random() * 5, 2) for i in range(tam)]
 
-print(lista)
-print(f'La potencia de los números impares es: {potencia}')
-print(f'La raíz de los números pares es: {raices}')
+print()
+aprobadas = [nota for nota in notas if nota >= 3]
+print ('Las calificaciones aprobadas son: ')
+print (aprobadas)
+print ()
+
+desaprobadas = [nota for nota in notas if nota < 3]
+print ('Las calificaciones desaprobadas son: ')
+print (desaprobadas)
+print ()
+
+rango1 = [x for x in notas if x < 2]
+print('0 a 1:', rango1)
+rango2 = [x for x in notas if 1 < x < 3]
+print('1 a 2:', rango2)
+rango3 = [x for x in notas if 2 < x < 4]
+print('2 a 3:', rango3)
+rango4 = [x for x in notas if 3 < x <= 5]
+print('3 a 4:', rango4)
+print()
+
+promedio_aprobadas = sum(aprobadas) / len(aprobadas)
+print(f"El promedio de notas aprobadas es: {promedio_aprobadas}")
+print ()
+
+promedio_desaprobadas = sum(desaprobadas) / len(desaprobadas)
+print(f"El promedio de notas desaprobadas es: {promedio_desaprobadas}")
+print()
