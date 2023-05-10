af7c3fe3eda3156a0b885ec28fbfcabf0518df22
ronald <ronaldpuerto65@gmail.com>
2023-05-10 17:21:16 -0500

    Correccion Funcion

--- Funciones/func.py
+++ Funciones/func.py
@@ -16,13 +16,22 @@ def promedioLista(lista):
     return sumaLista (lista)/len(lista)
 
 def mayor(lista1):
-    maximo = lista1[0]
+    maximo = 0
     for x in lista1:
         if x > maximo:
             maximo = x
     return maximo
 
+def menor(lista1):
+    minimo = 1000
+    for x in lista1:
+        if x < minimo:
+            minimo = x
+    return minimo
+
 print(lista1)
 print(f'La suma de la lista es: {sumaLista(lista1)}')
 print(f'El promedio es: {promedioLista(lista1)}')
 print(f'El numero mayor es: {mayor(lista1)}')
+print(f'El numero menor es: {menor(lista1)}')
+print(f'El orden ascendete de la lista es: {ordenasc(lista1)}')
