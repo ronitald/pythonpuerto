23e19a477a2c08984f9221fb99c2e7f94f3973bd
ronald <ronaldpuerto65@gmail.com>
2023-05-13 23:19:32 -0500

    Ejercicio de Funciones

--- Funciones/func.py
+++ Funciones/func.py
@@ -1,7 +1,7 @@
 import random
 
 def llenarLista(tam,rango):
-    lista=[random.randrange (rango) for i in range(tam)]
+    lista=[random.randrange(rango) for i in range(tam)]
     return lista
 
 lista1=llenarLista(5,20)
@@ -13,25 +13,42 @@ def sumaLista(lista):
     return sum
 
 def promedioLista(lista):
-    return sumaLista (lista)/len(lista)
+    return sumaLista(lista)/len(lista)
 
-def mayor(lista1):
+def mayor(lista):
     maximo = 0
-    for x in lista1:
+    for x in lista:
         if x > maximo:
             maximo = x
     return maximo
 
-def menor(lista1):
+def menor(lista):
     minimo = 1000
-    for x in lista1:
+    for x in lista:
         if x < minimo:
             minimo = x
     return minimo
 
+def ordenasc(lista):
+    for i in range(len(lista)-1):
+        for j in range(i+1, len(lista)):
+            if lista[i]>lista[j]:
+                lista[i],lista[j]=lista[j],lista[i]
+    return lista
+
+def ordendes(lista):
+    for i in range(len(lista)-1):
+        for j in range(i+1, len(lista)):
+            if lista[i]<lista[j]:
+                lista[i], lista[j]=lista[j], lista[i]
+    return lista
+
+print()
 print(lista1)
 print(f'La suma de la lista es: {sumaLista(lista1)}')
 print(f'El promedio es: {promedioLista(lista1)}')
 print(f'El numero mayor es: {mayor(lista1)}')
 print(f'El numero menor es: {menor(lista1)}')
 print(f'El orden ascendete de la lista es: {ordenasc(lista1)}')
+print(f'El orden descendente de la lista es: {ordendes(lista1)}')
+print()
