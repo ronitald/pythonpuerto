e15087e9049908956849433dfc22715e27b22039
ronald <ronaldpuerto65@gmail.com>
2023-05-29 17:05:29 -0500

    Modulos Y Paquetes - 29 Mayo

---
+++ Modulos/Fun1.py
@@ -0,0 +1,11 @@
+from sys import path
+
+path.append('C:\\pythonpuerto\\extra')
+
+import extra.iota
+import extra.good.alpha
+import extra.good.best.sigma
+
+print(extra.iota.FunI())
+print(extra.good.alpha.FunA())
+print(extra.good.best.sigma.FunS())
